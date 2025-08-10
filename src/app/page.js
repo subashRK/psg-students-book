bc140dec9165e6e425649bb86a965414ba643285
R.K.Subash <subashkohila24092007@gmail.com>
2025-08-10 15:06:20 +0530

    tentatively finished home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
-import Image from "next/image"
 import styles from "./home.module.css"
 import Link from "next/link"
 
-const IMG_WIDTH = 800
+const IMG_WIDTH = 1000
 
 export default function Home() {
 	return (
 		<div>
 			<main className={styles.container}>
 				<h1 className={styles.title}>
-					Get to know about your seniors and juniors studying in the{" "}
+					Get to know about your seniors and juniors studying in the,
+					<br />
 					<a href="https://www.psgtech.edu/" target="_blank">
 						PSG College of Technology!
 					</a>
@@ -19,12 +19,9 @@ export default function Home() {
 					Go to Students Book &rarr;
 				</Link>
 
-				<Image
-					src="/phone-notebook.jpg"
-					width={IMG_WIDTH}
-					height={IMG_WIDTH / 1.5} // 1.5 is the aspect ratio
-					alt="Phone book"
-				/>
+				<h2 style={{ opacity: 0.9, textAlign: "center" }}>
+					For the Students of PSG Tech, by a student.
+				</h2>
 			</main>
 		</div>
 	)
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import Link from "next/link"
 import styles from "./navbar.module.css"
+import Image from "next/image"
 
 const HEIGHT = 60
+const PROFILE_HEIGHT = 40
 
 export default function Navbar() {
 	return (
@@ -9,6 +11,21 @@ export default function Navbar() {
 			<nav style={{ height: HEIGHT }} className={styles.navbar}>
 				<Link href="/" className={styles.title}>
 					PSG Students Book
+				</Link>
+
+				<Link
+					href="https://github.com/subashRK"
+					className={styles.profile}
+					style={{ height: PROFILE_HEIGHT, width: PROFILE_HEIGHT }}
+					title="See Github Page"
+					target="_blank"
+				>
+					<Image
+						src="/github-profile.jpg"
+						height={PROFILE_HEIGHT}
+						width={PROFILE_HEIGHT}
+						alt="R.K.Subash"
+					/>
 				</Link>
 			</nav>
 
